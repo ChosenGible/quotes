1bc42d9fe9ec5ac1bf385d4de0b57f55942ade2d
Alex Brown <83.alex.72@gmail.com>
2020-03-02 16:59:07 +0000

    Commited everything I've done.

--- quotes.js
+++ quotes.js
@@ -1,4 +1,7 @@
 let personQuoted = "";
+let numQuoteTotal = 0;
+let numQuotesUsed = 0;
+let numQuotesWon = 0;
 
 function displayQuote(){
     let quoteBranchDecider = Math.round(Math.random());
@@ -6,30 +9,110 @@ function displayQuote(){
     let quoteBlock = document.getElementById("quote");
 
     if (quoteBranchDecider === 0){
-        quoteBlock.innerText = "hi";
         personQuoted = "Kanye"
+        let url = "https://api.kanye.rest";
+        fetch(url)
+            .then(function(response){
+                if (response.status != 200){
+                    return {
+                        text: "Error calling Kanye Api Service: " + response.statusText
+                    }
+                }
+                return response.json();
+            }).then(function(json){
+                quoteBlock.innerText = json.quote;
+            })
     }
     else {
-        quoteBlock.innerText = "test";
         personQuoted = "Trump";
+        let url = "https://www.tronalddump.io/random/quote";
+        fetch(url)
+            .then(function(response){
+                if (response.status != 200){
+                    return {
+                        text: "Error calling Trump Api Service: " + response.statusText
+                    }
+                }
+
+                return response.json();
+            }).then(function(json){
+                quoteBlock.innerText = json.value;
+            });
     }
+
+    console.log(personQuoted);
 
 }
 
-function checkQuote(e, answer){
-    console.log(e);
-    e.preventDefault();
+function displayEnding(){
+    let gamePage = "";
+    gamePage += "<h2> You got " + numQuotesWon + " of "  + numQuoteTotal + " quotes correct!!!</h2>";
+    gamePage += "<button id='playAgain'>Play Again</button>";
 
+    document.getElementById("page").innerHTML = gamePage;
+
+    console.log(document.getElementById("page"));
+}
+
+function displayStart(){
+    let gamePage = "";
+    gamePage += '<p class="imputPrompt">How many quotes would you like to try?</p>';
+    gamePage += '<select id="numQuotes">';
+    gamePage += '   <option value=1>1</option>';
+    gamePage += '   <option value=2>2</option>';
+    gamePage += '   <option value=3>3</option>';
+    gamePage += '   <option value=4>4</option>';
+    gamePage += '   <option value=5>5</option>';
+    gamePage += '</select>';
+    gamePage += '<button id="startGame">Start Game</button>';
+
+    document.getElementById("page").innerHTML = gamePage;
+}
+
+function checkButtons(e){
+    if (e.srcElement.id === 'donaldButton') {
+        checkQuote("Trump");
+    }
+    else if (e.srcElement.id === 'kanyeButton') {
+        checkQuote("Kanye");
+    }
+    else if (e.srcElement.id === 'playAgain'){
+        displayStart();
+    }
+    else if (e.srcElement.id === 'startGame'){
+        startGame();
+    }
+}
+
+function checkQuote(answer){
+    ++numQuotesUsed;
     if (answer === personQuoted){
         console.log("correct");
+        numQuotesWon += 1;
+        document.getElementById("score").innerText = numQuotesWon + " / " + numQuoteTotal;
+    }
+    else {
+        console.log("false");
     }
 
-    displayQuote();
+    if (numQuotesUsed >= numQuoteTotal){    
+        console.log("game finished");
+        displayEnding();
+    }
+    else {
+        displayQuote();
+    }
 }
 
-function startGame(e) {
+function startGameWrapper(e){
     e.preventDefault();
 
+    startGame();
+}
+
+function startGame() {
+    numQuotesWon = 0;
+    numQuotesUsed = 0;
     let numQuoteSelector = document.getElementById("numQuotes");
     let numQuotes = numQuoteSelector.options[numQuoteSelector.selectedIndex].value;
 
@@ -44,13 +127,9 @@ function startGame(e) {
     document.getElementById("page").innerHTML = gamePage;
 
     displayQuote();
-
-    console.log(personQuoted);
-
-    document.getElementById("kanyeButton").addEventListener('click', checkQuote(event, "kanye"));
-    document.getElementById("donaldButton").addEventListener('click', checkQuote(event, "Trump"));
-
+    numQuoteTotal = numQuotes;
 }
 
 
-document.getElementById("startGame").addEventListener('click', startGame);
+document.getElementById("startGame").addEventListener('click', startGameWrapper);
+document.getElementById("page").addEventListener('click', checkButtons);
